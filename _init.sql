4e22c1aeb691485639f3b69d23a5d35fd66fdfd8
adodo1 <adodo1@126.com>
2017-06-15 18:59:11 +0800

    commit

---
+++ _init.sql
@@ -0,0 +1,28 @@
+﻿
+-- 文件总表
+CREATE TABLE [FILES] (
+  [FID] CHAR(32), 
+  [FNAME] TEXT, 
+  [FSIZE] INT, 
+  [FPATH] TEXT, 
+  [FDATE] INT, 
+  [FLAG] INT, 
+  CONSTRAINT [sqlite_autoindex_FILES_1] PRIMARY KEY ([FID]));
+
+-- 文件的模板
+CREATE TABLE [TEMPLATE] (
+  [PID] CHAR(40), 
+  [FSTART] INT, 
+  [FEND] INT, 
+  [HEADSIZE] INT, 
+  [PTIME] INT);
+
+
+
+
+
+
+
+
+
+
--- fileupload_ok.py
+++ fileupload_ok.py
@@ -1,8 +1,70 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import os, io, sys, math, requests, json, re, base64, logging
+import time, ctypes, socket, threading, json, Queue, sqlite3, hashlib
+from threading import Thread
 
-#
+__mutex = threading.Lock()        # 线程锁
+
+
+##########################################################################
+##########################################################################
+##########################################################################
+class Worker(Thread):
+    # 线程池工作线程 只支持 python 2.7 或以上版本
+    worker_count = 0
+    def __init__(self, workQueue, resultQueue, timeout = 0, **kwds):
+       Thread.__init__(self, **kwds)
+       self.id = Worker.worker_count
+       Worker.worker_count += 1
+       self.setDaemon(True)
+       self.workQueue = workQueue
+       self.resultQueue = resultQueue
+       self.timeout = timeout
+       self.start()
+     
+    def run(self):
+        ''' the get-some-work, do-some-work main loop of worker threads '''
+        while True:
+            try:
+                callable, args, kwds = self.workQueue.get(timeout=self.timeout)
+                res = callable(*args, **kwds)
+                #print "worker[%2d]: %s" % (self.id, str(res))
+                self.resultQueue.put(res)
+            except Queue.Empty:
+                break
+            except :
+                print 'worker[%2d]' % self.id, sys.exc_info()[:2]
+
+class WorkerPool:
+    # 线程池
+    def __init__(self, num_of_workers=10, timeout = 1):
+        self.workQueue = Queue.Queue()
+        self.resultQueue = Queue.Queue()
+        self.workers = []
+        self.timeout = timeout
+        self._recruitThreads(num_of_workers)
+    def _recruitThreads(self, num_of_workers):
+        for i in range(num_of_workers): 
+            worker = Worker(self.workQueue, self.resultQueue, self.timeout)
+            self.workers.append(worker)
+    def wait_for_complete(self):
+        # ...then, wait for each of them to terminate:
+        while len(self.workers):
+            worker = self.workers.pop()
+            worker.join()
+            if worker.isAlive() and not self.workQueue.empty():
+                self.workers.append(worker)
+        #print "All jobs are are completed."
+    def add_job(self, callable, *args, **kwds):
+        self.workQueue.put((callable, args, kwds))
+    def get_result(self, *args, **kwds):
+        return self.resultQueue.get(*args, **kwds)
+
+
+##########################################################################
+##########################################################################
+##########################################################################
 class YunDisk:
     # 云盘 学习借鉴baidu pcs api
     # 块大小 字节
@@ -18,19 +80,60 @@ class YunDisk:
     HEADER_SIZE = len(BASE_DATA)
             
     
-    def __init__(self, cookie):
+    def __init__(self, cookie, conn):
         # 上传才需要cookie
         self._cookie = cookie
-        
+        self._conn = conn
         pass
 
     def Init(self):
         # 初始化
         pass
 
+
+            
+    def _addNewFile(self, fid, fname):
+        # 向数据库添加一个新文件信息
+        sql = 'select count(*) from FILES where FID=?'
+        args = (fid,)
+        cu = conn.cursor()
+        cu.execute(sql, args)
+        record = cu.fetchone()
+        count = record[0]
+        #
+        fsize = os.path.getsize(fname)
+        fpath, fname = os.path.split(fname)
+        fdate = int(time.time())
+        flag = 0
+        # 
+        if (count == 0):
+            # 添加一条文件记录
+            sql = 'insert into FILES(FID, FNAME, FSIZE, FPATH, FDATE, FLAG) values(?,?,?,?,?,?)'
+            args = (fid, fname, fsize, '/', fdate, flag)
+            cu.execute(sql, args)
+            
+        else:
+            # 更新记录
+            sql = 'update FILES set FID=?, FNAME=?, FSIZE=?, FPATH=?, FDATE=?, FLAG=? where FID=?'
+            args = (fid, fname, fsize, '/', fdate, flag, fid)
+            cu.execute(sql, args)
+        #
+
+        conn.commit()
+        cu.close()
+
     def UploadFile(self, fname):
         # 上传文件 返回文件fid
-        return self.UploadPart(fname, 0, -1)
+        # fname: 文件名
+        # 计算MD5值
+        fid = self._fileMD5(fname)
+        # 判断数据库里是否已经有
+        if (self._hasFile(fid)): return fid
+        # 没有的话创建一条记录和一张文件表
+        self._addNewFile(fid, fname)
+        
+        
+        #return self.UploadPart(fname, 0, -1)
         pass
 
     def UploadPart(self, fname, start, size):
@@ -174,11 +277,36 @@ class YunDisk:
         if (content_len == None): return False
         content_len = int(content_len)
         return content_len == size
-
     
+    def _fileMD5(self, fname):
+        # 计算文件MD5值
+        if not os.path.isfile(fname): return None
+        fhash = hashlib.md5()
+        f = file(fname, 'rb')
+        while True:
+            b = f.read(8192)
+            if not b : break
+            fhash.update(b)
+        f.close()
+        fmd5 = fhash.hexdigest()
+        logging.info('md5 - %s: %s', fmd5, fname)
+        return fmd5
+    
+    def _hasFile(self, fid):
+        # 判断数据库里是否已经有 如果有而且状态为[100]就跳过
+        sql = 'select FLAG from FILES where FID=?'
+        args = (fid,)
+        cu = conn.cursor()
+        cu.execute(sql, args)
+        record = cu.fetchone()
+        cu.close()
+        if (record): return record[0] == 100
+        else: return False
 
 
-
+##########################################################################
+##########################################################################
+##########################################################################
 if __name__ == '__main__':
     #
     print '[==DoDo==]'
@@ -203,7 +331,13 @@ if __name__ == '__main__':
 
     #
     cookie = open('./cookies/cookies.txt', 'r').read()
-    disk = YunDisk(cookie)
+    conn = sqlite3.connect('_disk.db', check_same_thread = False)
+    disk = YunDisk(cookie, conn)
+
+
+
+
+
     
     #data = open('data.zip', 'rb').read()
 
@@ -215,9 +349,11 @@ if __name__ == '__main__':
     #pid = disk._uploadData(data)
     #print pid
 
-    result = disk.UploadFile('./data/test.jpg')
+    #上传测试
+    #result = disk.UploadFile('./data/data.rar')
 
 
+    disk.UploadFile('./data/data.rar')
     
     logging.shutdown()
     print 'OK.'
